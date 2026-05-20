c59b3ac14399ccdba61968b3ee6dfb2ca5a8763a
krishna0890227 <s.kr.acharya@gmail.com>
2026-05-20 10:44:15 +0100

    arrowFunction added!

---
+++ arrowFunction.js
@@ -0,0 +1,43 @@
+// In this tutorial, we will discuss how arrow function is helpful
+// in the TypeScript applications. 
+
+let inputData = [0, 1, 3, 4, 5]
+
+// Tutorial 1: multiply inputData by 2 . 
+
+// let's go with for loop
+let results = [];
+for (let i=0; i<inputData.length; i++) {
+    results.push(inputData[i]*2);
+
+}
+console.log(results);
+
+// We need four line of code. 
+
+// let's go with map function. 
+
+const resultMap = inputData.map(function ( a) { return a*2});
+console.log(resultMap);
+
+// We can make more shorter. 
+const resultMap2 = inputData.map(a => a*2);
+console.log(resultMap2);
+
+
+// uses from implicit Functions; 
+
+const saySomething = message => console.log(message);
+saySomething("Hi there!")
+
+const sayHello = () => console.log("Say Hello!")
+sayHello();
+// display long message 
+
+const longMsg = () => (
+    `<p>
+    Hi, i am finally revise the tutorals. 
+    </p>`
+)
+
+console.log(longMsg());
